47b7e49fe1bd21930e020a2e5ac1666c45351851
Alice_space <alicespace.li@outlook.com>
2022-03-09 00:49:09 +0800

    feat: XRDemo with float panel

---
+++ src/components/ControlPanel.vue
@@ -0,0 +1,28 @@
+<template>
+  <a class="float">
+    CONTROL PANEL
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ControlPanel",
+});
+</script>
+
+<style scoped>
+.float {
+  position: fixed;
+  opacity: 70%;
+  width: 300px;
+  height: 300px;
+  top: 20px;
+  left: 20px;
+  background-color: #0c9;
+  color: #fff;
+  text-align: center;
+  box-shadow: 2px 2px 3px #999;
+}
+</style>
